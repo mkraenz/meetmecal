7a6e131a746b5d13a95bea82c5274a9809bc44c7
Mirco Kraenz <kraenzmirco@gmail.com>
2023-01-30 19:12:19 +0100

    fix: handle no slots available

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,14 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Button, Heading, SlideFade, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  Link,
+  SlideFade,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import type { FC } from "react";
+import { env } from "../env/client.mjs";
 import type { Slot } from "../state/app.context";
 import { useAppState } from "../state/app.context";
 import { formatDateNoTimezone, formatTimezone } from "../utils/date.utils";
@@ -10,6 +18,21 @@ interface Props {
   slots: Slot[];
   visible: boolean;
 }
+
+const NoSlotsAvailable: FC = () => {
+  const name = env.NEXT_PUBLIC_MY_FIRST_NAME;
+  const email = env.NEXT_PUBLIC_MY_COMPANY_EMAIL;
+  return (
+    <Text fontWeight={"bold"} p={12}>
+      No slots available. Please try a shorter meeting or contact {name}{" "}
+      directly by email at{" "}
+      <Link textDecoration={"underline"} href={`mailto://${email}`}>
+        {email}
+      </Link>
+      .
+    </Text>
+  );
+};
 
 const CalendarSlotButton: FC<{ slot: Slot }> = ({ slot }) => {
   const { dispatch } = useAppState();
@@ -40,20 +63,24 @@ const Calendar: FC<Props> = ({ slots, visible }) => {
         <Heading as="h4" size={"md"} mb={4}>
           {state.meetingType?.displayName}
         </Heading>
-        <Heading as="h5" size="sm" mb={4}>
-          {" "}
-          Select a start time.
-        </Heading>
-        <Text textColor={"secondaryText"}>
-          All times in {formatTimezone()} time
-        </Text>
-        {/* TODO handle no slots available */}
-        {slots.map((slot) => (
-          <CalendarSlotButton slot={slot} key={slot.start.getTime()} />
-        ))}
-        {/* <HStack justifyContent={"right"} minW="sm"> */}
+        {slots.length === 0 && <NoSlotsAvailable />}
+        {slots.length > 0 && (
+          <>
+            <Heading as="h5" size="sm" mb={4}>
+              {" "}
+              Select a start time.
+            </Heading>
+            <Text textColor={"secondaryText"}>
+              All times in {formatTimezone()} time
+            </Text>
+            {/* TODO handle no slots available */}
+            {slots.map((slot) => (
+              <CalendarSlotButton slot={slot} key={slot.start.getTime()} />
+            ))}
+          </>
+        )}
+
         <BackButton />
-        {/* </HStack> */}
       </VStack>
     </SlideFade>
   );
