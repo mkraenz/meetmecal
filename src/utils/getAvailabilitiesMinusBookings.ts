46945fa5e70b3c3773f3b241e3df10c26e737629
Mirco Kraenz <kraenzmirco@gmail.com>
2023-01-31 09:44:37 +0100

    feat: getAvailabilitiesMinusBookings returns the minimal intervals
    minimal in the sense that no two intervals overlap or are adjacent

--- src/utils/getAvailabilitiesMinusBookings.ts
+++ src/utils/getAvailabilitiesMinusBookings.ts
@@ -1,5 +1,6 @@
 import { DateTime, Interval } from "luxon";
 import type { AvailabilityEntity, BookingEntity } from "../server/db";
+import { last } from "./mymath";
 
 export const getAvailabilitiesMinusBookings = (
   availabilityEntities: AvailabilityEntity[],
@@ -24,12 +25,28 @@ export const getAvailabilitiesMinusBookings = (
         : a
     )
     .filter((a) => a.isValid && a.end > DateTime.now());
+
+  // merge availabilities that overlap
+  // 'maximal availabilitiy' in the sense that no two availabilities overlap or are adjacent
+  const maxAvs = futureAvs
+    .sort((a, b) => a.start.toMillis() - b.start.toMillis()) // earliest first
+    .reduce<Interval[]>((mergedAvs, next) => {
+      if (mergedAvs.length === 0) return [next];
+      // avs are valid (end > start) and
+      // sorted by start time (earliest first).
+      // Thus we just need to check for overlap on consecutive avs.
+      const previous = last(mergedAvs);
+      if (previous.overlaps(next)) {
+        return [...mergedAvs.slice(0, -1), previous.union(next)];
+      }
+      return [...mergedAvs, next];
+    }, []);
   const futureBookings = bookingsEntities
     .map((b) =>
       Interval.fromISO(`${b.start.toISOString()}/${b.end.toISOString()}`)
     )
     .filter((b) => b.end > DateTime.now());
-  const x = futureAvs.reduce<Interval[]>((newAvs, av) => {
+  const x = maxAvs.reduce<Interval[]>((newAvs, av) => {
     return [...newAvs, ...av.difference(...futureBookings)];
   }, []);
   return x;
--- src/utils/mymath.ts
+++ src/utils/mymath.ts
@@ -1,1 +1,3 @@
 export const range = (n: number) => [...Array(n).keys()];
+
+export const last = <T>(arr: T[]) => arr.at(-1) as T;
